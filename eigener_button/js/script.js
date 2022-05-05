efc1b9a2cd9346212db695935635cc47e2436a68
miagujer <104132051+miagujer@users.noreply.github.com>
2022-05-05 13:35:39 +0200

    erstes raufladen

---
+++ eigener_button/js/script.js
@@ -0,0 +1,26 @@
+console.log('godag')
+
+let n = 0; /*schlüsselwort let und variablenname. zuweisung des werts von rechts nach links */
+function buttonPressed() {
+    n++; /* ist dasselbe wie n=n+1*/
+    //console.log(document.getElementById("element"))
+    document.getElementById("element").style.left = (n*5) + "px";
+}
+
+function neuesElement() {
+    let div = document.createElement("div");
+    div.classList.add('random');
+    div.style.left = Math.random() * window.innerWidth + "px";
+    div.style.top = Math.random() * window.innerHeight + "px";
+    document.body.appendChild(div);
+}
+
+function neuesBild() {
+    n++;
+   let versatz = 20; /* um wieviel das bild jedesmal nach rechts rückt*/
+   let myImage = new Image(100, 100);
+   myImage.src = 'assets/images/jackson.png';
+   myImage.classList.add('bild');
+   myImage.style.left = n * versatz + "px";
+   document.body.appendChild(myImage);
+}
